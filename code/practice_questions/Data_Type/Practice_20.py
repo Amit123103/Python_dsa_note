1061b3faff49da73f6d33bf41308c47ff4d758a0
Amit123103 <amitakhil001@gmail.com>
2026-07-11 00:25:27 +0530

    Initial commit

--- code/practice_questions/Data_Type/Practice_20.py
+++ code/practice_questions/Data_Type/Practice_20.py
@@ -4,3 +4,11 @@ Write a Python program that iterates over elements as many times as its count.
 
 Sample Output: ['p', 'p', 'p', 'p', 'q', 'q']'''
 
+# Import the Counter class from the collections module
+from collections import Counter
+
+# Create a Counter object 'c' with specified initial counts for keys 'p', 'q', 'r', and 's'
+c = Counter(p=4, q=2, r=0, s=-2)
+
+# Print the elements in the Counter 'c' as a list
+print(list(c.elements())) 
