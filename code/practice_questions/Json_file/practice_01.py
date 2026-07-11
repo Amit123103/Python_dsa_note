dc4ab6f1c434ad40dc5b79cf7cb29f6afbc41081
Amit123103 <amitakhil001@gmail.com>
2026-07-12 00:34:30 +0530

    Initial commit

--- code/practice_questions/Json_file/practice_01.py
+++ code/practice_questions/Json_file/practice_01.py
@@ -1,1 +1,24 @@
 '''1. Write a Dictionary to a JSON File (Most Common)'''
+
+# import json
+
+# student = {
+#     "name": "Amit",
+#     "age": 22,
+#     "course": "Math",
+#     "marks": 95
+# }
+
+# with open("student.json", "w") as file:
+#     json.dump(student, file, indent=4)
+
+# print("JSON file created Successfully")
+
+import json
+
+with open("student.json", "r") as file:
+    data = json.load(file)
+
+print(data)
+print(data["name"])
+print(data["age"])
