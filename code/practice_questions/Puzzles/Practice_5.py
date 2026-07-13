6d8cdcb3170f6ee25d6012a81fd9951eb5b53bbf
Amit123103 <amitakhil001@gmail.com>
2026-07-13 21:03:39 +0530

    Initial commit

--- code/practice_questions/Puzzles/Practice_5.py
+++ code/practice_questions/Puzzles/Practice_5.py
@@ -18,3 +18,53 @@ Input:
 Output:
 True
 '''
+
+# Define a function named 'test' that takes a list of strings 'str1' as input
+def test(str1):
+    # Check if the second-to-last character of the last string in 'str1' is a proper substring of the last string
+    # and if the second-to-last character is different from the last character
+    return str1[len(str1) - 2] in str1[len(str1) - 1] and str1[len(str1) - 2] != str1[len(str1) - 1]
+
+# Create a list of strings 'str11' with specific elements
+str11 = ["a", "abb", "sfs", "oo", "de", "sfde"]
+
+# Print the original list
+print("Original list:")
+print(str11)
+
+# Print the result of the test function applied to the 'str11' list
+print("Check the nth-1 string is a proper substring of nth string of the said list of strings:")
+print(test(str11))
+
+# Create a different list of strings 'str11' with specific elements
+str11 = ["a", "abb", "sfs", "oo", "ee", "sfde"]
+
+# Print the original list
+print("\nOriginal list:")
+print(str11)
+
+# Print the result of the test function applied to the modified 'str11' list
+print("Check the nth-1 string is a proper substring of nth string of the said list of strings:")
+print(test(str11))
+
+# Create another list of strings 'str11' with specific elements
+str11 = ["a", "abb", "sad", "ooaa", "esdfe", "sfsdfde", "sfsd", "sfsdf", "qwrew"]
+
+# Print the original list
+print("\nOriginal list:")
+print(str11)
+
+# Print the result of the test function applied to the modified 'str11' list
+print("Check the nth-1 string is a proper substring of nth string of the said list of strings:")
+print(test(str11))
+
+# Create one more list of strings 'str11' with specific elements
+str11 = ["a", "abb", "sad", "ooaa", "esdfe", "sfsdfde", "sfsd", "sfsdf", "qwsfsdfrew"]
+
+# Print the original list
+print("\nOriginal list:")
+print(str11)
+
+# Print the result of the test function applied to the modified 'str11' list
+print("Check the nth-1 string is a proper substring of nth string of the said list of strings:")
+print(test(str11))
