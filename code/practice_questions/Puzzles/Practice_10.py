e117c761ffe7475c972e10cf1951c11399812642
Amit123103 <amitakhil001@gmail.com>
2026-07-13 21:07:41 +0530

    Initial commit

---
+++ code/practice_questions/Puzzles/Practice_10.py
@@ -0,0 +1,13 @@
+'''10. Split Matched Parentheses Groups
+
+Given a string consisting of whitespace and groups of matched parentheses, write a Python program to split it into groups of perfectly matched parentheses without any whitespace.
+Input:
+( ()) ((()()())) (()) ()
+Output:
+['(())', '((()()()))', '(())', '()']
+Input:
+() (( ( )() ( )) ) ( ())
+Output:
+['()', '((()()()))', '(())']
+'''
+
