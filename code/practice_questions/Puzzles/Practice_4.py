150c5edb24f8084a0a82c5b66c8446431f9146b9
Amit123103 <amitakhil001@gmail.com>
2026-07-13 21:02:40 +0530

    Initial commit

--- code/practice_questions/Puzzles/Practice_4.py
+++ code/practice_questions/Puzzles/Practice_4.py
@@ -14,3 +14,57 @@ Input: 17
 Output:
 [17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
 '''
+
+
+# Define a function named 'test' that takes an integer 'n' as input
+def test(n):
+    # Use a list comprehension to generate a list of values: n + 2 * i for i in the range from 0 to n-1
+    return [n + 2 * i for i in range(n)]
+
+# Assign a specific integer 'n' to the variable
+n = 2
+
+# Print the number of piles
+print("Number of piles:", n)
+
+# Print the header for the output
+print("Number of stones in each pile:")
+
+# Print the result of the test function applied to the integer 'n'
+print(test(n))
+
+# Assign a different integer 'n' to the variable
+n = 10
+
+# Print the number of piles
+print("\nNumber of piles:", n)
+
+# Print the header for the output
+print("Number of stones in each pile:")
+
+# Print the result of the test function applied to the modified integer 'n'
+print(test(n))
+
+# Assign another integer 'n' to the variable
+n = 3
+
+# Print the number of piles
+print("\nNumber of piles:", n)
+
+# Print the header for the output
+print("Number of stones in each pile:")
+
+# Print the result of the test function applied to the modified integer 'n'
+print(test(n))
+
+# Assign yet another integer 'n' to the variable
+n = 17
+
+# Print the number of piles
+print("\nNumber of piles:", n)
+
+# Print the header for the output
+print("Number of stones in each pile:")
+
+# Print the result of the test function applied to the modified integer 'n'
+print(test(n))
